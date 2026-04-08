d9a25a04c45e8a61ce79f48237f749155b271b4b
Abraham Epton <abraham.epton@gmail.com>
2026-04-07 20:24:38 -0700

    Lots more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import { Global } from "@emotion/react";
 import { ReactFlowProvider } from "reactflow";
 import DocumentFlow from "./components/DocumentFlow";
-
-const globalStyles = {
-  body: {
-    margin: 0,
-    padding: 0,
-    fontFamily: '"EB Garamond", Georgia, serif',
-  },
-};
+import { useFontStore } from "./store/fontStore";
 
 function App() {
+  const fontFamily = useFontStore((s) => s.current.family);
+
   return (
     <>
-      <Global styles={globalStyles} />
+      <Global
+        styles={{
+          body: {
+            margin: 0,
+            padding: 0,
+            fontFamily,
+          },
+          "button:hover:not(:disabled)": {
+            background: "rgba(0,0,0,0.04) !important",
+          },
+          "select:hover": {
+            background: "rgba(0,0,0,0.04) !important",
+          },
+        }}
+      />
       <ReactFlowProvider>
         <DocumentFlow />
       </ReactFlowProvider>
